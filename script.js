41ed38ef642affa2b2ab99ab4c3f538a15260993
Musa Kaan Altın <mkaanaltin@gmail.com>
2024-11-25 14:59:36 +0300

    Updated site with experience details

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function showSection(sectionId) {
-    // Öncelikle tüm bölümleri gizleyelim
+    // Öncelikle tüm bölümleri gizle
     const sections = document.querySelectorAll('.section');
     sections.forEach(section => {
-      section.style.display = 'none';
+      section.style.display = 'none';  // Bölümleri gizle
+      section.style.opacity = 0;       // Opaklık sıfırla
     });
   
-    // Tıklanan bölümü gösterelim
+    // Tıklanan bölümü göster
     const section = document.getElementById(sectionId);
-    section.style.display = 'block';
+    section.style.display = 'block';   // Bölümü göster
+    section.style.opacity = 1;         // Opaklık sıfırla
   }
   
